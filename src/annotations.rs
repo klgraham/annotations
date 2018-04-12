484c5301f3e87b4a406c74d5d12f3ca1c691c86f
Kenneth Graham <klgraham@gmail.com>
2018-04-11 22:43:01 -0500

    Added comments are renamed metadata to overlay

--- src/annotations.rs
+++ src/annotations.rs
@@ -1,15 +1,11 @@
-//This module describes a span of text that may or may not have associated
-//annotations. For example, a sentence can have multiple REGION_CLASS annotations
-//and a token can have a POS annotation.
+/// This module describes a span of text that can have annotations
+/// overlaid on top of it.
 
 use std::string::ToString;
 use std::rc::Rc;
 
 
-// todo: There can be annotations on top of more than just text
-// The text of a document goes here. The content struct owns the
-// text of the document. Anything else that uses this text will
-// have to borrow only.
+/// Raw text content that can be annotated
 pub struct Content {
     pub text: String
 }
@@ -20,33 +16,38 @@ impl ToString for Content {
     }
 }
 
+
+/// The types of annotations that can be added on top of the text in `Content`
 #[derive(Debug, PartialEq)]
 pub enum AnnotationType {
     Highlight,
     Comment(String)
 }
 
-// an annotation on the text
+
+// An annotation on top of the text in `Content`
 pub struct Annotation {
     pub start: usize,
     pub length: usize,
-    pub metadata: AnnotationType,
-    pub (crate) parent: Rc<Content>// The `Content` on which this annotation is overlaid
+    pub overlay: AnnotationType,
+    /// The `Content` on which this annotation is overlaid
+    parent: Rc<Content>
 }
 
 impl Annotation {
+    /// Gets the span of text that is being annotated.
     pub fn text(&self) -> &str {
         &self.parent.text[self.start..(self.start + self.length)]
     }
 
     pub fn new(start: usize, length: usize, metadata: AnnotationType, parent: &Rc<Content>) -> Annotation {
-        Annotation {start, length, metadata, parent: Rc::clone(parent)}
+        Annotation {start, length, overlay: metadata, parent: Rc::clone(parent)}
     }
 }
 
 impl ToString for Annotation {
     fn to_string(&self) -> String {
-        format!("Annotation(text: {}, metadata: {:?})", self.text(), self.metadata)
+        format!("Annotation(text: {}, metadata: {:?})", self.text(), self.overlay)
     }
 }
 
@@ -56,17 +57,16 @@ mod tests {
 
     #[test]
     fn can_create_annotations() {
-        let text = "Destiny 2 is a video game on Microsoft Xbox.".to_string();
+        let text = String::from("Destiny 2 is a video game on Microsoft Xbox.");
         let content = Rc::new(Content {text});
 
-        let md1 = AnnotationType::Comment("The best gaming system.".to_string());
+        let md1 = AnnotationType::Comment(String::from("The best gaming system."));
 
         let annotation1 = Annotation::new(29, 14, md1, &content);
-
         let annotation2 = Annotation::new(0, 9, AnnotationType::Highlight, &content);
 
         assert_eq!(annotation1.text(), "Microsoft Xbox");
         assert_eq!(annotation2.text(), "Destiny 2");
-        assert_eq!(annotation1.metadata, AnnotationType::Comment("The best gaming system.".to_string()));
+        assert_eq!(annotation1.overlay, AnnotationType::Comment(String::from("The best gaming system.")));
     }
 }
