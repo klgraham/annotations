f0d3747305d6178f54ea7f57ef8d45bec0328aed
Ken Graham <klgraham@gmail.com>
2019-03-02 14:52:53 -0500

    Changed module structure in hopes of being able to support images, audio, or video

---
+++ src/content/mod.rs
@@ -0,0 +1,7 @@
+//! This module defines types of content that can be annotated.
+
+
+pub mod text;
+
+// TODO: implement image module using https://github.com/PistonDevelopers/image
+
--- src/content/text.rs
+++ src/content/text.rs
@@ -1,17 +1,22 @@
 //! This module describes a span of text that can have annotations overlaid
 //! on top of it.
 
+
+// text.rs
+
 use std::rc::Rc;
 use std::fmt;
 use chrono::{DateTime, Utc};
+use std::collections::HashMap;
 
 
-/// Raw text content that can be annotated
+/// Text that can be annotated
 pub struct Content {
     pub text: String
 }
 
-// Writes Content to stdout
+
+// Format Content
 impl fmt::Display for Content {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.text)
@@ -19,7 +24,7 @@ impl fmt::Display for Content {
 }
 
 
-/// The types of annotations that can be added on top of the text in `Content`
+/// The types of annotations that can be added to `Content`
 #[derive(Debug, PartialEq)]
 pub enum AnnotationType {
     Highlight,
@@ -29,7 +34,7 @@ pub enum AnnotationType {
 }
 
 
-/// An annotation on top of the text in `Content`
+/// An annotation on top of `Content`
 pub struct Annotation {
     /// Index where the `Annotation` starts in `Content.text`.
     pub start: usize,
@@ -43,6 +48,7 @@ pub struct Annotation {
     parent: Rc<Content>
 }
 
+
 impl Annotation {
     /// Gets the span of text that is being annotated.
     pub fn text(&self) -> &str {
@@ -55,12 +61,34 @@ impl Annotation {
     }
 }
 
+
 // Writes an Annotation to stdout
 impl fmt::Display for Annotation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Annotation(text: {}, overlay: {:?}, timestamp: {})", self.text(), self.overlay, self.timestamp)
     }
 }
+
+
+/// Raw text and a collection of annotations overlaid on top of the text
+pub struct AnnotatedContent {
+    pub content: Rc<Content>,
+    pub annotations: HashMap<usize, Annotation>
+}
+
+
+impl AnnotatedContent {
+    pub fn new(text: String) -> AnnotatedContent {
+        let content = Rc::new(Content {text});
+        let annotations: HashMap<usize, Annotation> = HashMap::new();
+        AnnotatedContent {content, annotations}
+    }
+
+    pub fn add_annotation(&mut self, a: Annotation) {
+        self.annotations.insert(a.start, a);
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -94,5 +122,25 @@ mod tests {
         assert_eq!(annotation2.text(), "Destiny 2");
         assert_eq!(annotation1.overlay, AnnotationType::Comment(String::from("The best gaming system.")));
         assert_eq!(annotation2.overlay, AnnotationType::Comment(String::from("The best first-person shooter, to date.")));
+    }
+
+    #[test]
+    fn can_create_annotated_content() {
+        let text = "Mary had a little lamb.".to_string();
+        let ac = AnnotatedContent::new(text);
+
+        assert_eq!(&ac.content.text, "Mary had a little lamb.");
+    }
+
+    #[test]
+    fn can_add_annotations() {
+        let text = "Mary had a little lamb.".to_string();
+        let mut ac = AnnotatedContent::new(text);
+        let person = Annotation::new(0, 4, AnnotationType::Highlight, &ac.content);
+
+        ac.add_annotation(person);
+
+        assert_eq!(&ac.content.text, "Mary had a little lamb.");
+        assert_eq!(ac.annotations.len(), 1usize);
     }
 }
